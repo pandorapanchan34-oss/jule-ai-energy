119d5ddf4d4c59f0ab0b4ec1f70230688baae812
pandorapanchan34-oss <pandorapanchan34@gmail.com>
2026-04-11 07:27:25 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,29 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  // index.htmlがあるフォルダを指定
   root: 'demo',
+  
+  // 公開設定
+  base: './',
+
   build: {
+    // 出力先を外側の dist フォルダに設定
     outDir: '../dist',
     emptyOutDir: true,
     sourcemap: true,
     minify: 'terser',
   },
+
   resolve: {
     alias: {
+      // ソースコード(src)へのパスを通す
       '@': resolve(__dirname, './src'),
     },
   },
+
   server: {
     port: 3000,
-    strictPort: true,
     host: true,
   }
 });
